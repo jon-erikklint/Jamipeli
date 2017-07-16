e7c48330cc5f2e27669a271652dab25cdaf63944
jorre jorre <jon-erik.klint@luukku.com>
2017-07-16 12:19:18 +0300

    uita ja baseball

--- Jamipeli/Assets/Scripts/Bullet.cs
+++ Jamipeli/Assets/Scripts/Bullet.cs
@@ -52,7 +52,6 @@ public class Bullet : MonoBehaviour {
                 health.Damage(damage);
             Destroy(gameObject);
         }
-        Debug.Log((Time.time - lastHit > safeTime) + ", " + IsFatal());
         lastHit = Time.deltaTime;
     }
 
---
+++ Jamipeli/Assets/Scripts/IO/Controllers/SlowAmountController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SlowAmountController : MonoBehaviour {
+
+    private PlayerMover player;
+
+    private Text text;
+
+    void Start()
+    {
+        this.player = FindObjectOfType<PlayerMover>();
+        this.text = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        text.text = "Global slows: " + player.globalSlows;
+    }
+}
